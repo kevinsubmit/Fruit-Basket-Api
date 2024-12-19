3fc01a53f7369545f0e1c75a19abc9200b09f64e
austin li <645267294@qq.com>
2024-12-19 11:30:19 -0500

    modify some files

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const testJWTRouter = require('./controllers/test-jwt');
 const usersRouter = require('./controllers/users');
 const profilesRouter = require('./controllers/profiles');
 
@@ -17,10 +16,10 @@ app.use(cors());
 app.use(express.json());
 
 // Routes go here
-app.use('/test-jwt', testJWTRouter);
+
 app.use('/users', usersRouter);
 app.use('/profiles', profilesRouter);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log('The express app is ready!');
 });
