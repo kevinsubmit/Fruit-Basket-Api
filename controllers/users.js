8472f52636b4d9d727549d7fb519c68e8bd9efc8
austin li <645267294@qq.com>
2024-12-19 21:01:48 -0500

    finish user

--- controllers/users.js
+++ controllers/users.js
@@ -5,15 +5,6 @@ import User from "../models/user.js";
 import jwt from "jsonwebtoken";
 
 const SALT_LENGTH = 12;
-\
-router.post("/signup", async (req, res) => {
-  try {
-  const { username, password, role } = req.body;
-    // Check if the username is already taken
-    const userInDatabase = await User.findOne({ username: req.body.username });
-    if (userInDatabase) {
-      return res.json({ error: "Username already taken." });
-
 router.post("/signup", async (req, res, next) => {
   try {
     const { username, password, role } = req.body;
@@ -21,32 +12,11 @@ router.post("/signup", async (req, res, next) => {
     const userInDatabase = await User.findOne({ username: req.body.username });
     if (userInDatabase) {
       return res.json({ error: "Username already taken" });
-
     }
     // Create a new user with hashed password
     const user = await User.create({
       username,
       hashedPassword: bcrypt.hashSync(password, SALT_LENGTH),
-
-      role:role ||"guest"         //new add
-    });
-    const token = jwt.sign(
-      { username: user.username, _id: user._id },
-      process.env.JWT_SECRET
-    );
-    res.status(201).json({ user, token });
-  } catch (error) {
-    console.log(111);
-    res.status(400).json({
-      message: error.message,
-      data: {},
-      status: 405,
-    });
-  }
-});
-
-
-
       role: role,
     });
     const token = jwt.sign(
@@ -62,34 +32,23 @@ router.post("/signup", async (req, res, next) => {
   }
 });
 
-
 router.post("/signin", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
     if (user && bcrypt.compareSync(req.body.password, user.hashedPassword)) {
       const token = jwt.sign(
-
-        { username: user.username, _id: user._id },
-        process.env.JWT_SECRET
-      );
-      res.status(200).json({ token });
-
         { username: user.username, _id: user._id, role: user.role },
         process.env.JWT_SECRET,
         { expiresIn: "1h" }
       );
       res.status(200).json({data:{user,token}});
-
     } else {
       res.status(401).json({ error: "Invalid username or password." });
     }
-
-    res.status(400).json({ error: error.message });
-
+  } catch (error) {
     error.status = 400;
     error.errorCode = "BAD_REQUEST";
     next(error);
-
   }
 });
 
